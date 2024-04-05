65e84b61cf8f9f55322d7538afcd82531911e689
Uyen Nguyen <nguyen_u1@denison.edu>
2024-04-04 22:56:20 -0400

    update base + dense derive class

--- Project6/DenseGraph.cpp
+++ Project6/DenseGraph.cpp
@@ -3,31 +3,42 @@
 using namespace std;
 
 // Default constructor
-DenseGraph::DenseGraph(void) {
-
+DenseGraph::DenseGraph(void) : Graph(DEFAULT_VERTICES_NUM, 0) {
+    adjMatrix.resize(V, vector<int>(V, -1));        // Initialize adjacency matrix with -1
 } 
 
 // Copy constructor
-
+DenseGraph::DenseGraph(const DenseGraph& other) : Graph(other) {
+    adjMatrix = other.adjMatrix;
+}
 
 // Constructor with specifies V and E
+DenseGraph::DenseGraph(int vertices, int edges) : Graph(vertices, 0) {
+    adjMatrix.resize(V, vector<int>(V, -1));        // Initialize adjacency matrix with -1
+}
 
 // Destructor
+DenseGraph::~DenseGraph(void) {
+    V = E = 0;
+}
 
 // Assignment operator
-
-
-
-
+DenseGraph& DenseGraph::operator=(const DenseGraph& other) {
+    if (this != &other) {
+        Graph::operator=(other);        // Call base class assignment operator to copy V and E
+        adjMatrix = other.adjMatrix;    // Deep copy the adjacent matrix
+    }
+    return *this;
+}
 
 bool DenseGraph::isEdge(int v1, int v2) const {
-    if (v1 < 0 || v1 >= V || v2 < 0 || v2 > V) 
+    if (v1 < 0 || v1 >= V || v2 < 0 || v2 >= V) 
         throw std::invalid_argument("Invalid vertex number.");
     return adjMatrix[v1][v2] != -1;
 }
 
 int DenseGraph::getWeight(int v1, int v2) const {
-    if (v1 < 0 || v1 >= V || v2 < 0 || v2 > V)
+    if (v1 < 0 || v1 >= V || v2 < 0 || v2 >= V)
         throw std::invalid_argument("Invalid vertex number.");
     if (adjMatrix[v1][v2] == -1) 
         throw std::runtime_error("Edge does not exist.");
@@ -53,3 +64,13 @@ void DenseGraph::insertEdge(int v1, int v2, int w = 1) {
     adjMatrix[v1][v2] = adjMatrix[v2][v1] = w;
 }
 #endif
+
+void DenseGraph::print(ostream& os) const {
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (adjMatrix[i][j] != -1) {
+                os << i << " " << j << " " << adjMatrix[i][j] << endl;
+            }
+        }
+    }
+}
---
+++ Project6/SparseGraph.cpp
@@ -0,0 +1,14 @@
+#include "SparseGraph.h"
+#include <stdexcept>
+#include <vector>
+using namespace std;
+
+SparseGraph::SparseGraph(void) {
+    V = DEFAULT_VERTICES_NUM;
+    E = 0;
+
+}
+
+
+
+
